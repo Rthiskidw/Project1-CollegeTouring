02caa8dc2cf12acda94aa1f173e0f0ccc6a01a7f
Rthiskidw <reedwilsonmusic@gmail.com>
2021-02-16 16:31:49 -0800

    adding sql changes

--- database.h
+++ database.h
@@ -2,6 +2,7 @@
 #define DATABASE_H
 
 #include <QMainWindow>
+#include <QtSql>
 
 namespace Ui {
 class database;
@@ -15,8 +16,12 @@ public:
     explicit database(QWidget *parent = nullptr);
     ~database();
 
+private slots:
+    void on_pushButton_clicked();
+
 private:
     Ui::database *ui;
+    QSqlDatabase myDataBase;
 };
 
 #endif // DATABASE_H
