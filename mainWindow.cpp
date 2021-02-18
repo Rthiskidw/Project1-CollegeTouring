4ea1fc440d7d1ab27b586b5477aa20e4730e1c5f
Alankvuong <alankvuong@gmail.com>
2021-02-17 18:14:16 -0800

    new file:   .gitignore 	new file:   Project1-College_Touring.pro 	new file:   README.md 	new file:   choosepath.cpp 	new file:   choosepath.h 	new file:   choosepath.ui 	new file:   database.cpp 	new file:   img/ChoosePath imgs/college tour.png 	new file:   img/ChoosePath imgs/road image.png 	new file:   img/ChoosePath imgs/school.png 	new file:   img/Images.qrc 	new file:   img/mainwindow imgs/college_tour_text.png 	new file:   img/mainwindow imgs/mainwindow_picture.png 	new file:   main.cpp 	new file:   mainWindow.cpp 	new file:   mainWindow.h 	new file:   mainWindow.ui

---
+++ mainWindow.cpp
@@ -0,0 +1,23 @@
+#include "mainWindow.h"
+#include "ui_mainwindow.h"
+//#include "login.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    //this is me adding a comment ok
+}
+
+void MainWindow::on_actionLogin_triggered()
+{
+//    auto* login = newLogin(this);
+//    login->show();
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
