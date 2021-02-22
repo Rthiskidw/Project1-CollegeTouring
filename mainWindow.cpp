5f39f4f6ed89b230956a64fcb4a3049ccd252051
Alankvuong <alankvuong@gmail.com>
2021-02-21 18:20:04 -0800

    added a button_clicked slot method

--- mainWindow.cpp
+++ mainWindow.cpp
@@ -1,6 +1,6 @@
 #include "mainWindow.h"
-#include "ui_mainwindow.h"
-//#include "login.h"
+#include "ui_mainWindow.h"
+#include "choosepath.h"
 #include "database.h"
 
 
@@ -12,14 +12,16 @@ MainWindow::MainWindow(QWidget *parent)
     //this is me adding a comment ok
 }
 
-void MainWindow::on_actionLogin_triggered()
-{
-//    auto* login = newLogin(this);
-//    login->show();
-}
-
 MainWindow::~MainWindow()
 {
     delete ui;
 }
 
+void MainWindow::on_startButton_clicked()
+{
+    MainWindow* mainwindow = new MainWindow(this);
+    mainwindow->show();
+    mainwindow->hide();
+    auto* choosePath = new ChoosePath(this);
+    choosePath->show();
+}
