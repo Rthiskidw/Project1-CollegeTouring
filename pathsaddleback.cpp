ef2351c038e334ab48933881f3ac5187c53c6571
Rthiskidw <reedwilsonmusic@gmail.com>
2021-03-03 11:41:27 -0800

    ouputing schools to the saddlebackpath option along with distances from saddleback

--- pathsaddleback.cpp
+++ pathsaddleback.cpp
@@ -9,6 +9,7 @@ pathSaddleback::pathSaddleback(QWidget *parent) :
     ui(new Ui::pathSaddleback)
 {
     ui->setupUi(this);
+    initializeList();
 }
 
 pathSaddleback::~pathSaddleback()
@@ -24,7 +25,19 @@ void pathSaddleback::on_startTrip_button_clicked()
 
 void pathSaddleback::on_planTrip_button_clicked()
 {
-    // Need to implement planTrip button
+    QWidget *container = new QWidget;
+    QVBoxLayout *vBoxLayout = new QVBoxLayout;
+
+    container->setLayout(vBoxLayout);
+
+    ui->scrollArea_chosenSchools->setWidget(container);
+
+    //filling scroll area with chosen schools
+    for(int i = 0; i < chosenSchoolsLabels.size(); i++)
+    {
+        vBoxLayout->addWidget(chosenSchoolsLabels[i]);
+    }
+
 
 }
 
@@ -36,3 +49,40 @@ void pathSaddleback::on_backButton_clicked()
     choosePath -> show();
 
 }
+
+void pathSaddleback::initializeList()
+{
+    QSqlQuery *query = new QSqlQuery();
+
+    schoolLabelsVector.clear();
+
+    QWidget *container = new QWidget;
+    QVBoxLayout *vBoxLayout = new QVBoxLayout;
+
+    container->setLayout(vBoxLayout);
+
+    ui->scrollArea_SchoolOptions->setWidget(container);
+
+
+    query->prepare("SELECT ending_college, distances  FROM Colleges WHERE starting_college='Saddleback College'");
+    if(!query->exec())
+    {
+        qDebug() << "pathSaddleback initialize school list query failed";
+    }
+    else
+    {
+        while(query->next())
+        {
+             //adding college options and their distances from saddleback to check boxes
+             QLabel* school = new QLabel((query->value(0).toString()) + " [" + (query->value(1).toString()) + " Miles From Saddleback]");
+             schoolLabelsVector.push_back(school);
+        }
+
+        for(int i = 0; i < schoolLabelsVector.size(); i++)
+        {
+            vBoxLayout->addWidget(schoolLabelsVector[i]);
+        }
+    }
+}
+
+
