2071c8b82ec5a66c1d95cde571348d9bde6c4561
RyanSolanki <73560558+RyanSolanki@users.noreply.github.com>
2021-03-05 16:40:42 -0800

    Added to CustomTrip

--- souvenirshop.cpp
+++ souvenirshop.cpp
@@ -1,17 +1,120 @@
 #include "souvenirshop.h"
 #include "ui_souvenirshop.h"
+#include <QMessageBox>
+#include <QVBoxLayout>
 
 souvenirShop::souvenirShop(QVector<QString> collegesVector, QWidget *parent) :
     QWidget(parent),
     ui(new Ui::souvenirShop)
 {
     ui->setupUi(this);
-    ui->label_collegeName->setText(collegesVector[0]);
+    selectedColleges = collegesVector;
+    ui->label_collegeName->setText(selectedColleges[collegeCount]);
 
+    QSqlQueryModel* model=new QSqlQueryModel();
 
+    QSqlQuery* qry=new QSqlQuery();
+
+    qry->prepare("SELECT souvenirs, cost FROM Souvenirs WHERE college= (:college)");
+    qry->bindValue(":college", selectedColleges[collegeCount]);
+
+    if(qry->exec())
+    {
+        qDebug() << "Souvenirs updated";
+    }
+
+    model->setQuery(*qry);
+
+    ui->souvenir_tableView->setModel(model);
+    ui->souvenir_tableView->setColumnWidth(0, 195);
+
+    collegeCount++;
 }
 
 souvenirShop::~souvenirShop()
 {
     delete ui;
+}
+
+void souvenirShop::on_nextCollege_button_clicked()
+{
+    if(collegeCount < selectedColleges.size())
+    {
+        ui->label_collegeName->setText(selectedColleges[collegeCount]);
+
+        QSqlQueryModel* model=new QSqlQueryModel();
+
+        QSqlQuery* qry=new QSqlQuery();
+
+        qry->prepare("SELECT souvenirs, cost FROM Souvenirs WHERE college= (:college)");
+        qry->bindValue(":college", selectedColleges[collegeCount]);
+
+        if(qry->exec())
+        {
+            qDebug() << "Souvenirs updated";
+        }
+
+        model->setQuery(*qry);
+
+        ui->souvenir_tableView->setModel(model);
+        ui->souvenir_tableView->setColumnWidth(0, 195);
+
+        collegeCount++;
+    }
+    else
+    {
+        QMessageBox::information(this, "Warning", "Your tour has ended. To continue, please click \"End Tour\"");
+    }
+}
+
+void souvenirShop::on_endTour_button_clicked()
+{
+
+}
+
+void souvenirShop::on_souvenir_tableView_clicked(const QModelIndex &index)
+{
+    if(index.isValid())
+    {
+        int row = index.row();
+        tempSouvenir = index.sibling(row, 0).data().toString();
+        souvenirCost = index.sibling(row, 1).data().toString().replace("$", "").toDouble();
+        cost = index.sibling(row, 1).data().toString();
+
+        qDebug() << tempSouvenir << Qt::endl << souvenirCost << Qt::endl;
+    }
+}
+
+void souvenirShop::on_buy_button_clicked()
+{
+    QSqlQueryModel* model=new QSqlQueryModel();
+
+    QSqlQuery* qry=new QSqlQuery();
+
+    QLabel *souvenirName = new QLabel(tempSouvenir + "\t\t" + cost);
+    purchasedSouvenirs.push_back(souvenirName);
+    QWidget *container = new QWidget;
+    QVBoxLayout *vBoxLayout = new QVBoxLayout;
+
+    container->setLayout(vBoxLayout);
+
+    ui->scrollArea_purchased->setWidget(container);
+
+    //filling scroll area with chosen schools
+    for(int i = 0; i < purchasedSouvenirs.size(); i++)
+    {
+        vBoxLayout->addWidget(purchasedSouvenirs[i]);
+    }
+
+    if(qry->exec())
+    {
+        qDebug() << "Souvenirs updated";
+    }
+
+    model->setQuery(*qry);
+
+    //collegeTotals[collegeCount] = collegeTotals[collegeCount] + souvenirCost;
+    grandTotal = grandTotal + souvenirCost;
+    //souvenirQuants[collegeCount]++;
+
 }
--- souvenirshop.h
+++ souvenirshop.h
@@ -3,6 +3,10 @@
 
 #include <QWidget>
 #include <QVector>
+#include <QSqlQueryModel>
+#include <QSqlQuery>
+#include <QDebug>
+#include <QLabel>
 
 namespace Ui {
 class souvenirShop;
@@ -16,13 +20,28 @@ public:
     explicit souvenirShop(QVector<QString> collegeVector, QWidget *parent = nullptr);
     ~souvenirShop();
 
+private slots:
+    void on_nextCollege_button_clicked();
+
+    void on_endTour_button_clicked();
+
+    void on_souvenir_tableView_clicked(const QModelIndex &index);
+
+    void on_buy_button_clicked();
+
 private:
     Ui::souvenirShop *ui;
 
     double distanceTraveled = 0;    // total distance traveled during trip
     double grandTotal = 0;          // grand total spent on souvenirs
+    int collegeCount = 0;           // Used for vector index to keep track of which college to display
+    QVector<QString> selectedColleges; // Names of each selected college
     QVector<double> collegeTotals;  // total for each college
     QVector<int> souvenirQuants;    // souvenir quantities
+    QVector<QLabel*> purchasedSouvenirs;
+    QString tempSouvenir;
+    QString cost;
+    double souvenirCost;
 };
 
 #endif // SOUVENIRSHOP_H
