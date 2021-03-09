c916999b13c93b3956c61137ad7692000b1a174b
RyanSolanki <73560558+RyanSolanki@users.noreply.github.com>
2021-03-08 21:11:14 -0800

    Finished ASU Trip

--- pathasu.h
+++ pathasu.h
@@ -4,8 +4,8 @@
 #include "souvenirshop.h"
 #include "QVBoxLayout"
 #include <QWidget>
-#include <qcheckbox.h>
 #include <QList>
+#include <QMessageBox>
 #include <QDialog>
 
 namespace Ui {
@@ -21,21 +21,28 @@ public:
     ~pathASU();
     void initializeList();
     void CheckboxChanged();
+    void efficiencyAlgo(QVector<QString> *colleges,
+                     QVector<QString> *routeNames,
+                     QVector<double> *routeDistances,
+                     QString currentCollege);
 
 private slots:
     void on_backButton_clicked();
 
     void on_pushButton_planTrip_clicked();
 
+    void on_toolButton_startTrip_clicked();
+
 private:
     Ui::pathASU *ui;
-    QVector<QCheckBox*> checkBoxVector;
     QVector<QLabel*> orderedSchoolsLabels;
     QVector<QString> chosenSchools;
     QVector<QString> orderedSchoolsName;
     QVector<double> orderedSchoolsDistance;
     Database myDb = Database(PROJECT_PATH + "/college.db");
     double totalDistance = 0;
+    int collegeCount = 1;
+    int userInput = 0;
 };
 
 #endif // PATHASU_H
